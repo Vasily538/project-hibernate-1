7f1849437b895c60b1f3156a187b452cc77fb57e
Анкудинов <mybox_@mail.ru>
2024-05-16 10:30:30 +0300

    2

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,17 +1,26 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import com.sun.java.swing.plaf.windows.TMSchema;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Repository;
 
 import jakarta.annotation.PreDestroy;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
+    private final SessionFactory sessionFactory;
 
     public PlayerRepositoryDB() {
+        Properties properties = new Properties();
 
+        sessionFactory = new Configuration()
+                .buildSessionFactory();
     }
 
     @Override
